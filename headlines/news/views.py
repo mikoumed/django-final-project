1242538df0151fae621c4c9c5c8d648229f9c1f8
mikoumed <mikoumed87@gmail.com>
2019-01-25 01:00:37 +0100

    categories template added

--- headlines/news/views.py
+++ headlines/news/views.py
@@ -10,16 +10,18 @@ class IndexPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            categories = self.request.user.categories
-            languages = self.request.user.languages
-            
-        try:
-            response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'.format(country='us', api_key='d380b7d83c074b4e8b710d302cc53601')).json()
-        except requests.exceptions.RequestException as e:
-            return e
+            categories = self.request.user.categories.all()
+            languages = self.request.user.languages.all()
+            result = {}
+            for category in categories:
+                try:
+                    response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
+                                country='us', category=category.name, api_key='d380b7d83c074b4e8b710d302cc53601')).json()
+                except requests.exceptions.RequestException as e:
+                    return e
 
-        context['count'] = response['totalResults']
-        context['articles'] = response['articles']
+                result[category.name] = response['articles']
+            context['articles'] = result
 
         return context
         #
