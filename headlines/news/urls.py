f7c7fb4235f38842978dc7a19df49a6e795a9218
mikoumed <mikoumed87@gmail.com>
2019-01-30 10:37:48 +0100

    update index template

--- headlines/news/urls.py
+++ headlines/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('index', views.IndexPageView.as_view(), name='index'),
+    path('json', views.json_response, name='json_response')
     ]
--- headlines/news/views.py
+++ headlines/news/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 import requests
+from users.models import Language, Category
+from django.http import JsonResponse
 
 # Create your views here.
+COUNTRIES = {
+    'english' : 'us',
+    'french' : 'fr',
+    'spanish' : 'sa'
+}
 
 class IndexPageView(TemplateView):
     template_name = 'index.html'
@@ -12,18 +19,37 @@ class IndexPageView(TemplateView):
         if self.request.user.is_authenticated:
             categories = self.request.user.categories.all()
             languages = self.request.user.languages.all()
-            result = {}
-            for category in categories:
-                try:
-                    response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
-                                country='us', category=category.name, api_key='d380b7d83c074b4e8b710d302cc53601')).json()
-                except requests.exceptions.RequestException as e:
-                    return e
+            results = {}
+            for language in languages:
+                results.setdefault(language.name, {})
+                for category in categories:
+                    try:
+                        response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
+                                    country=COUNTRIES[language.name], category=category.name, api_key='d380b7d83c074b4e8b710d302cc53601')).json()
+                    except requests.exceptions.RequestException as e:
+                        return e
 
-                result[category.name] = response['articles']
-            context['articles'] = result
+                    results[language.name].setdefault(category.name,[])
+                    results[language.name][category.name] = response['articles']
+            context['payload'] = results
+            return context
 
-        return context
+def json_response(request):
+    categories = Category.objects.all()
+    languages = Language.objects.all()
+    context = {}
+    for language in languages:
+        context.setdefault(language.name, {})
+        for category in categories:
+            try:
+                response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
+                            country=COUNTRIES[language.name], category=category.name, api_key='d380b7d83c074b4e8b710d302cc53601')).json()
+            except requests.exceptions.RequestException as e:
+                return e
+
+            context[language.name].setdefault(category.name,[])
+            context[language.name][category.name] = response['articles']
+    return JsonResponse(context, safe=False)
         #
         # for category in categories:
         #     response = newsapi.get_top_headlines(category=category)
