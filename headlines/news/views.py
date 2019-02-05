2bddc2aec3f445a62e7f0c73f0a48f0b0acc1c7c
mikoumed <mikoumed87@gmail.com>
2019-02-05 12:49:11 +0100

    using session to avoid loading new data <15min

--- headlines/news/views.py
+++ headlines/news/views.py
@@ -4,6 +4,7 @@ import requests
 from users.models import Country, Category
 from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import datetime, timedelta
 
 COUNTRIES = {
     'United States' : 'us',
@@ -18,22 +19,33 @@ class IndexPageView(TemplateView, LoginRequiredMixin):
     news_api_key = 'd380b7d83c074b4e8b710d302cc53601'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         if self.request.user.is_authenticated:
             categories = self.request.user.categories.all()
             countries = self.request.user.countries.all()
             results = {}
-            for country in countries:
-                results.setdefault(country.name, {})
-                for category in categories:
-                    try:
-                        response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
-                                    country=COUNTRIES[country.name], category=category.name, api_key='d380b7d83c074b4e8b710d302cc53601')).json()
-                    except requests.exceptions.RequestException as e:
-                        return e
+            def request_api():
+                for country in countries:
+                    results.setdefault(country.name, {})
+                    for category in categories:
+                        try:
+                            response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
+                                        country=COUNTRIES[country.name], category=category.name, api_key=self.news_api_key)).json()
+                        except requests.exceptions.RequestException as e:
+                            return e
 
-                    results[country.name].setdefault(category.name,[])
-                    results[country.name][category.name] = response['articles']
-            context['payload'] = results
+                        results[country.name].setdefault(category.name,[])
+                        results[country.name][category.name] = response['articles']
+                context['payload'] = results
+                self.request.session['last_result'] = results
+                self.request.session['last_time'] = datetime.now().strftime('%H%M%S')
+
+            if 'last_time' not in self.request.session:
+                request_api()
+            elif datetime.now() - datetime.strptime(self.request.session['last_time'] ,'%H%M%S') <= timedelta(minutes=15):
+                request_api()
+            else:
+                context['payload'] = self.request.session['last_result']
             return context
 
 # def json_response(request):
--- headlines/users/models.py
+++ headlines/users/models.py
@@ -8,12 +8,20 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 
 class Country(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
             return self.name
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
 
 
 class User(AbstractUser):
