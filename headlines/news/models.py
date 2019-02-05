45ee34c7f574ec12b2f39b1b809b666648f4b7b3
mikoumed <mikoumed87@gmail.com>
2019-02-05 12:50:42 +0100

    removing news model

--- headlines/news/models.py
+++ headlines/news/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class New(models.Model):
-    
-    title = models.CharField(max_length=250)
-    source = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    content = models.TextField()
