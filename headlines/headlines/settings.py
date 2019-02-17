df49298e421942a062c5feac602515a70fe0f69a
mikoumed <mikoumed87@gmail.com>
2019-02-17 16:01:06 +0100

    celery set up

--- headlines/headlines/settings.py
+++ headlines/headlines/settings.py
@@ -135,3 +135,26 @@ STATICFILES_DIRS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
+
+# CELERY/REDIS settings
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Casablanca'
+CELERY_RESULT_BACKEND='redis://localhost:6379/1'
+CELERY_TASK_IGNORE_RESULT= -1
+
+
+
+# REDIS CACHE settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
--- headlines/news/views.py
+++ headlines/news/views.py
@@ -1,18 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-import requests
 from users.models import Country, Category
-from django.http import JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from datetime import datetime, timedelta
+from django.core.cache import cache
+from pprint import pprint
+import requests
+# from django.shortcuts import render
+# from django.http import JsonResponse
 
-COUNTRIES = {
-    'United States' : 'us',
-    'France' : 'fr',
-    'Australia' : 'au',
-    'Great Britain': 'gb',
-    'Germany' : 'de'
-}
 
 class IndexPageView(TemplateView, LoginRequiredMixin):
     template_name = 'index.html'
@@ -21,31 +15,8 @@ class IndexPageView(TemplateView, LoginRequiredMixin):
         context = super().get_context_data(**kwargs)
 
         if self.request.user.is_authenticated:
-            categories = self.request.user.categories.all()
-            countries = self.request.user.countries.all()
-            results = {}
-            def request_api():
-                for country in countries:
-                    results.setdefault(country.name, {})
-                    for category in categories:
-                        try:
-                            response = requests.get('https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'.format(
-                                        country=COUNTRIES[country.name], category=category.name, api_key=self.news_api_key)).json()
-                        except requests.exceptions.RequestException as e:
-                            return e
-
-                        results[country.name].setdefault(category.name,[])
-                        results[country.name][category.name] = response['articles']
-                context['payload'] = results
-                self.request.session['last_result'] = results
-                self.request.session['last_time'] = datetime.now().strftime('%H%M%S')
-
-            if 'last_time' not in self.request.session:
-                request_api()
-            elif datetime.now() - datetime.strptime(self.request.session['last_time'] ,'%H%M%S') <= timedelta(minutes=15):
-                request_api()
-            else:
-                context['payload'] = self.request.session['last_result']
+            result = cache.get(self.request.user.id)
+            context['payload'] = result
             return context
 
 # def json_response(request):
