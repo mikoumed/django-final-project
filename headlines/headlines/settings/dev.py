eefb9188396e91e1f8d8084d6ac7ba6e8143740a
mikoumed <mikoumed87@gmail.com>
2019-03-03 23:34:08 +0100

    settings updated

--- headlines/headlines/settings/dev.py
+++ headlines/headlines/settings/dev.py
@@ -1,1 +1,34 @@
 from .base import *
+
+
+# CELERY/REDIS settings
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Casablanca'
+CELERY_RESULT_BACKEND='redis://localhost:6379/1'
+CELERY_TASK_IGNORE_RESULT= -1
+
+
+
+# REDIS CACHE settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# CELERY BEAT SCHEDULER
+CELERY_BEAT_SCHEDULE = {
+    'request_api_every_15_min': {
+        'task': 'tasks.request_api',
+        'schedule': 900.0,
+    },
+}
+
+CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
--- headlines/headlines/settings/prod.py
+++ headlines/headlines/settings/prod.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+import urlparse
 from .base import *
 
 SECRET_KEY = os.getenv(
@@ -10,3 +11,47 @@ DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ['*']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+# CELERY/REDIS settings
+BROKER_URL = os.environ['REDIS_URL']
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Casablanca'
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+CELERY_TASK_IGNORE_RESULT= -1
+
+redis_url = urlparse.urlparse(os.environ.get('REDIS_URL'))
+
+# REDIS CACHE settings
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://localhost:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+CACHES = {
+        'default': {
+            'BACKEND': 'redis_cache.RedisCache',
+            'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+            'OPTIONS': {
+                'PASSWORD': redis_url.password,
+                'DB': 1,
+        }
+    }
+}
+
+# CELERY BEAT SCHEDULER
+CELERY_BEAT_SCHEDULE = {
+    'request_api_every_15_min': {
+        'task': 'tasks.request_api',
+        'schedule': 900.0,
+    },
+}
+
+CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler'
