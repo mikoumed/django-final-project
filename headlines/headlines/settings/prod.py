4fdd7fed42f26e928cba097e2e1c372adc00ba0c
mikoumed <mikoumed87@gmail.com>
2019-03-04 20:57:15 +0100

    sdf

--- headlines/headlines/settings/prod.py
+++ headlines/headlines/settings/prod.py
@@ -26,6 +26,9 @@ CELERY_TASK_IGNORE_RESULT= -1
 
 redis_url = urlparse(os.environ.get('REDIS_URL'))
 
+# worker: celery -A headlines worker
+# beat: celery -A headlines beat
+
 # REDIS CACHE settings
 # CACHES = {
 #     "default": {
